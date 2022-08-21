2c18b84b7be7b2d767daed3f0f22062c5d724e58
MangGlang <71730799+MangGlang@users.noreply.github.com>
2022-08-20 23:25:04 -0700

    client rating

--- scriptName.js
+++
@@ -1,5 +0,0 @@
-var el = styles.getElementById("#heart__shape"); // or use $0 in chrome;
-do {
-  var styles = window.getComputedStyle(el);
-  console.log(styles.zIndex, el);
-} while (el.parentElement && (el = el.parentElement));
